4909f21f672dd32b2014d5af7d93315372ec4262
SteffanCrowley <steffancrow@gmail.com>
2022-07-13 16:42:22 -0700

    Added dummydata

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,14 @@
 const graphql = require("graphql");
 
+//dummydata
+let userData = [
+  { id: "1", name: "Bond", age: 36 },
+  { id: "33", name: "Fred", age: 33 },
+  { id: "343", name: "Michael", age: 23 },
+  { id: "5435", name: "Jeff", age: 76 },
+  { id: "654653", name: "James", age: 25 },
+];
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -29,6 +38,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
 
       resolve(parent, args) {
+        let user = {
+          id: "345",
+          age: 33,
+          name: "Steffan",
+        };
+
+        return user;
+
         //we resolve with data
         //get and return data from a datasource
       },
