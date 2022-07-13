69c2ecc3293c9b81f8b5731a51e89037568ee27e
SteffanCrowley <steffancrow@gmail.com>
2022-07-13 16:26:16 -0700

    Completed first schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,41 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+} = graphql;
 
 //create type
 const UserType = new graphql.GraphQLObjectType({
   name: "User",
   description: "Documentation for user...",
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
   }),
+});
+
+//RootQuery
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  description: "Description",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+
+      resolve(parent, args) {
+        //we resolve with data
+        //get and return data from a datasource
+      },
+    },
+  },
+});
+
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
 });
